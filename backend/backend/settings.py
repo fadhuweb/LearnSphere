575ea60bc7497e3599a1cf97a23138bfb6e07c2c
Fadhuweb <fadhlullah532@gmail.com>
2025-03-26 21:09:47 +0100

    fadh

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,25 +92,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if USE_SQLITE:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME', 'elearning_db'),
-            'USER': os.getenv('DB_USER', 'elearning_user'),
-            'PASSWORD': os.getenv('DB_PASSWORD', 'Umarfaruq123'),
-            'HOST': os.getenv('DB_HOST', 'localhost'),
-            'PORT': os.getenv('DB_PORT', '5432'),
-        }
-    }
-
+# if USE_SQLITE:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.getenv('DB_NAME', 'elearning_db'),
+#             'USER': os.getenv('DB_USER', 'elearning_user'),
+#             'PASSWORD': os.getenv('DB_PASSWORD', 'Umarfaruq123'),
+#             'HOST': os.getenv('DB_HOST', 'localhost'),
+#             'PORT': os.getenv('DB_PORT', '5432'),
+#         }
+#     }
+DATABASES = {
+    'default': dj_database_url.parse("postgresql://neondb_owner:npg_X8nCoqdHz2hZ@ep-aged-water-a5qd3hr2-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
+}
 
 DATABASE_ROUTERS = ['backend.db_router.DatabaseRouter']
 
